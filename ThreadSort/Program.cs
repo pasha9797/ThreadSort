497d4500453b0592c5d1a4184178996b2baa288f
pasha9797 <pasha.97.97@mail.ru>
2017-03-23 13:27:14 +0300

    Добавьте файлы проекта.

---
+++ ThreadSort/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                int maxThreads, amount;
+                int[] array;
+                bool logs;
+                string time;
+
+                Console.Write("Insert amount of elements: ");
+                amount = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Insert maximal amount threads: ");
+                maxThreads = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Insert 'y' to do logs and other key not to do logs: ");
+                logs = Console.ReadLine() == "y";
+
+                Console.WriteLine();
+                Random rand = new Random();
+                array = new int[amount];
+                for (int i = 0; i < amount; i++)
+                {
+                    array[i] = rand.Next(-50, 50);
+                    Console.Write(array[i] + " ");
+                }
+                int[] secondArray = new int[amount];
+                array.CopyTo(secondArray, 0);
+                Console.WriteLine();
+
+                Console.Write('\n' + "Press any key to start sort...;");
+                Console.ReadKey();
+
+                Console.Write("\n\n");
+                time = DateTime.Now.ToString("hh:mm:ss:fff");
+                Sort.ShellSort(array, maxThreads, logs);
+                Console.WriteLine(time);
+                Console.WriteLine(DateTime.Now.ToString("hh:mm:ss:fff"));
+                Console.Write('\n');
+
+                Console.Write("Sorting with usual Shell sort...\n\n");
+                Console.WriteLine(DateTime.Now.ToString("hh:mm:ss:fff"));
+                Sort.ShellSortUsual(secondArray);
+                Console.WriteLine(DateTime.Now.ToString("hh:mm:ss:fff"));
+
+                Console.WriteLine();
+                for (int i = 0; i < amount; i++)
+                {
+                    Console.Write(array[i] + " ");
+                }
+                Console.WriteLine();
+
+                Console.Write('\n' + "Press any key to exit...;");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ ThreadSort/Sort.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadSort
+{
+    public static class Sort
+    {
+
+        private static List<Thread> threadList = new List<Thread>();
+        private static bool doLogs;
+        private static Semaphore sem;
+
+        public static void ShellSort(int[] array, int maxThreads, bool logs)
+        {
+            doLogs = logs;
+            for (int k = array.Length / 2; k > 0; k /= 2)
+            {
+                if (doLogs) Console.WriteLine("["+ DateTime.Now.ToString("hh:mm:ss:fff")+"] "+"Step {0} begin\n", k);
+                sem = new Semaphore(maxThreads, maxThreads);
+                threadList.Clear();
+                for (int i = 0; i < k; i++)
+                {
+                    Thread thread = new Thread((id) =>
+                    {
+                        sem.WaitOne();
+                        
+                        if (doLogs) Console.WriteLine("[" + DateTime.Now.ToString("hh:mm:ss:fff") + "] "+"[Thread {0}] Start", (int)id);
+
+                        for (int j = (int)id + k; j < array.Length; j += k)
+                        {
+                            for (int z = j; z >= k && array[z - k] > array[z]; z -= k)
+                            {
+                                int temp = array[z];
+                                array[z] = array[z - k];
+                                array[z - k] = temp;
+                            }
+                        }
+                        if (doLogs) Console.WriteLine("[" + DateTime.Now.ToString("hh:mm:ss:fff") + "] "+"[Thread {0}] End", (int)id);
+                        sem.Release();
+                    });
+                    threadList.Add(thread);
+                    //thread.Start(i);
+                    
+                }
+                for (int i = 0; i < threadList.Count; i++)
+                {
+                    threadList[i].Start(i);
+                }
+                foreach (Thread thread in threadList)
+                {
+                    thread.Join();
+                }
+                
+                if (doLogs) Console.WriteLine("[" + DateTime.Now.ToString("hh:mm:ss:fff") + "] "+"Step {0} end\n", k);
+
+            }
+        }
+
+        public static void ShellSortUsual(int[] array)
+        {
+            int step = array.Length / 2;
+            while (step > 0)
+            {
+                int i, j;
+                for (i = step; i < array.Length; i++)
+                {
+                    int value = array[i];
+                    for (j = i - step; (j >= 0) && (array[j] > value); j -= step)
+                    {
+                        array[j + step] = array[j];
+                    }
+                    array[j + step] = value;
+                }
+                step /= 2;
+            }
+        }
+    }
+}
